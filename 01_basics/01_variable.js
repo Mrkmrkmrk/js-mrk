c300519c6b2f7832bf5d550420358df311ecfdcc
Mrkmrkmrk <manjunathkumbar666@gmail.com>
2025-04-23 18:47:22 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 12222
+let accountEmail ="manju.com"
+var accountPassword = "1234566"
+accountCity = "jaipur"
+let accountState;
+
+// acoountId=2
+accountEmail="mrk2.com"
+accountPassword="4333434"
+accountCity="sirsi"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version 
+
+// alert("hello ");//we are using  node.js not a browser
+// main documentaion in java script are in tc3 and ecma browser 
+
+
+
+//types of data types 
+let sting1="manjunath"//string
+let num=3//integer
+let its_you=false//boolean true /false
+//null->is a standalone
+let tem=null;
+
+let hel;
+//undefined
+//symbol=>used for unique
+
+
+console.log(string1);
+
+//object
+console.log(typeof null);
+console.log(typeof its_you);
+
