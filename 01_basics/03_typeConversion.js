f45f0edabe473d064e25f47d7174be0908f93855
Mrkmrkmrk <manjunathkumbar666@gmail.com>
2025-04-24 19:06:56 +0000

    git commit

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,50 @@
+let type= true;//not a number//
+let conversion="hello"
+// console.log(typeof(type));
+// console.log(typeof(conversion));
+
+let typecon=Number( type);
+// console.log(typeof typecon);
+// console.log( typecon);
+
+
+
+
+let booleanNum=1
+let booleanCon=Boolean(booleanNum)
+// console.log(booleanCon );
+
+let mrk=33
+let stringCon=String(mrk);
+// console.log(stringCon);
+// console.log(typeof (stringCon));
+
+
+// ---------------------------operation---------------------------------------------
+let val=3
+let negVal=-val
+
+
+// console.log(negVal);
+
+
+//  console.log(2+2);
+//  console.log(2-2);
+//   console.log(2*3);
+//    console.log(10**6);
+//     console.log(10/6);
+//     console.log(16%8);
+
+//     let str1="mrk";
+//     let str2=" king";
+//     console.log(str1 + str2);
+
+//     console.log("1"+2)
+//     console.log(2+"2")    
+//     console.log("1"+2+2)
+//     console.log(2+2+"3")
+
+
+
+    console.log(+true);
+    console.log(+null);
